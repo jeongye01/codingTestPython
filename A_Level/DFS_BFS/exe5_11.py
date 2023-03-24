fd1ae054d8a11dc94cfece2d052db53ea1b765b3
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-24 23:34:56 +0900

    Update exe5_11.py

--- A_Level/DFS_BFS/exe5_11.py
+++ A_Level/DFS_BFS/exe5_11.py
@@ -3,7 +3,7 @@ from collections import deque
 # N, M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
 # 2차원 리스트의 맵 정보 입력받기
-graph = []
+graph = []  
 for i in range(n):
  graph.append(list(map(int, input())))
 # 이동할 네 방향 정의(상, 하, 좌, 우)
