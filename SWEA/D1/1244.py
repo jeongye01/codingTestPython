f3c03a64bd9569bdd681576d10cc81ec4a3d339f
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-28 09:30:32 +0900

    #swea 최대 상금

--- SWEA/D1/1244.py
+++ SWEA/D1/1244.py
@@ -11,12 +11,14 @@ for test_case in range(1, T + 1):
     idxs=[0]*len(array)
     for i in range(len(array)):
       idxs[i]=i
-
+    #두개를 뽑는 모든 조합
     comb = list(combinations(idxs, 2))
     prod=[]
+    # i개의 조합에서만 위치 변경되는 경우 pick
     for i in range(1,m+1):
       prod=prod+list(combinations(comb, i))
     result=[]
+    # 각 경우에 대해서 result 도출
     for p in prod:
       tmp=array[:]
       for i in range(m):
@@ -25,7 +27,7 @@ for test_case in range(1, T + 1):
       result.append(''.join(tmp))
       
       
-    
+    # 가장 큰 값 
     ans=max(result)
 
     print("#{} {}".format(test_case,ans))
