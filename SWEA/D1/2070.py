fb934b03130d9f74b121486c32c9949eab106c3c
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-28 11:16:54 +0900

    #swea 큰 놈, 작은 놈, 같은 놈

---
+++ SWEA/D1/2070.py
@@ -0,0 +1,19 @@
+#큰 놈, 작은 놈, 같은 놈
+
+#내 풀이->테스트 통과 DATE=>4.28 풀이시간 2분 
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    n,m=map(int,input().split())
+    ans=""
+    if(n<m):
+        ans="<"
+    elif(n>m):
+        ans=">"
+    else:
+        ans="="
+
+
+
+    
+    print("#{} {}".format(test_case,ans))
