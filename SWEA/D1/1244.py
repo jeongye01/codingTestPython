0855eb7a16b172f8eba7503c36053bb33cd3332b
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-28 09:28:33 +0900

    #swea 최대 상금

---
+++ SWEA/D1/1244.py
@@ -0,0 +1,31 @@
+#[S/W 문제해결 응용] 2일차 - 최대 상금
+#내 풀이->테스트 통과 DATE=>4.28 풀이시간 40분 
+from itertools import combinations
+
+
+T = int(input())
+for test_case in range(1, T + 1):
+    n,m=map(int,input().split())
+    ans=0
+    array=list(str(n))
+    idxs=[0]*len(array)
+    for i in range(len(array)):
+      idxs[i]=i
+
+    comb = list(combinations(idxs, 2))
+    prod=[]
+    for i in range(1,m+1):
+      prod=prod+list(combinations(comb, i))
+    result=[]
+    for p in prod:
+      tmp=array[:]
+      for i in range(m):
+        c=p[i%len(p)]
+        tmp[c[0]],tmp[c[1]]= tmp[c[1]],tmp[c[0]]
+      result.append(''.join(tmp))
+      
+      
+    
+    ans=max(result)
+
+    print("#{} {}".format(test_case,ans))
