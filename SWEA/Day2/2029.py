e5100339f63850f7bcf78381569d55b8c85cb49c
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-01 23:01:59 +0900

    #swea 몫과 나머지 출력하기

---
+++ SWEA/Day2/2029.py
@@ -0,0 +1,10 @@
+# 몫과 나머지 출력하기 
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+
+  n,m=map(int,input().split())
+
+        
+    
+  print("#{} {} {}".format(test_case,n//m,n%m))
