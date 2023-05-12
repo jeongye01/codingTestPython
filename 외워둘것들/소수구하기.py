c00d313018c075964ae173312526ae098aa1a2ac
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-12 20:49:11 +0900

    Create 소수구하기.py

---
+++ 외워둘것들/소수구하기.py
@@ -0,0 +1,11 @@
+import math
+def prime_check(n):
+    if n == 1 :
+        return False
+    for div in range(2,int(math.sqrt(n))+1):
+        if n%div == 0:
+            return False
+    return True
+for i in range(1,1000000):
+  if prime_check(i):
+    print(i,end=" ")
