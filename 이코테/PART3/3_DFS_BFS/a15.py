2ee77d69a9fb9c4e79fe215a40dee00896f70e0e
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-13 18:45:20 +0900

    #이코테 감시피하기 재풀이

--- 이코테/PART3/3_DFS_BFS/a15.py
+++ 이코테/PART3/3_DFS_BFS/a15.py
@@ -64,3 +64,71 @@ print(answer)
 
 
 '''
+
+##내풀이 -> 테스트 통과  DATE->5.13 풀이시간->60분 ->입력방식에 주의하자!!!!
+'''
+from itertools import combinations
+
+n=int(input())
+board=[]
+idxs=[]
+dx=[0,-1,0,1]
+dy=[-1,0,1,0]
+def check(x,y,d):
+
+  global visited,board
+
+  if x<0 or x>=n or y<0 or y>=n:
+    #print("?",x,y,len(board))
+    return True
+  elif board[y][x]=="S":
+    #print("S",x,y)
+    return False
+  elif board[y][x]=="O":
+    #print("O",x,y,board[y][x])
+    return True
+
+  return check(x+dx[d],y+dy[d],d)
+
+def solution():
+  for i in range(n):
+    for j in range(n):
+      #print(i,j,"tlqkf?",n,board[i][j])
+      if board[i][j]=="T":
+          #print(i,j,"선생",n)
+          for d in range(4):
+            if not check(j,i,d):
+              #print(j,i,"미통과")
+              return False
+       
+  return True
+    
+  
+for i in range(n):
+  data=list(input().split())
+  for j in range(n):
+    if data[j]=="X":
+      idxs.append((j,i))
+  board.append(data)
+  
+comb=list(combinations(idxs,3))
+ans="NO"
+for c in comb:
+  for o in c:
+    tx,ty=o
+    board[ty][tx]="O"
+  #for b in board:
+   #   print(*b)
+  if solution()==True:
+    ans="YES"
+    
+    break
+  for o in c:
+    tx,ty=o
+    board[ty][tx]="X"
+    
+
+print(ans)
+
+
+'''
