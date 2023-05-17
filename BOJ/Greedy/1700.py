604b32e9511d67370c4d67e719b7e33545545b5a
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-18 04:07:53 +0900

    #BOJ 회의실배정

---
+++ BOJ/Greedy/1700.py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/1931
+#회의실배정
+##내 풀이->테스트 통과 DATE=>5.17 풀이시간 60분 
+n=int(input())
+meetings=[]
+
+for _ in range(n):
+  st,ed=map(int,input().split())
+  meetings.append([st,ed])
+meetings.sort(key=lambda x:(x[0],x[1]))
+#for m in meetings:
+ # print(m)
+
+cnt=1
+last=0
+for i in range(1,n):
