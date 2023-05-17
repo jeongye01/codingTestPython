572cf357a8903ddc12f2e0bf6e84453deaba0e00
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-18 04:08:19 +0900

    Update 1700.py

--- BOJ/Greedy/1700.py
+++ BOJ/Greedy/1700.py
@@ -14,3 +14,10 @@ meetings.sort(key=lambda x:(x[0],x[1]))
 cnt=1
 last=0
 for i in range(1,n):
+  if meetings[last][1]<=meetings[i][0] :
+    cnt+=1
+    last=i
+  elif meetings[last][1]>=meetings[i][1] :
+    last=i
+
+print(cnt)
