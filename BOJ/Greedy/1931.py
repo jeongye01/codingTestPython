b92af2e537e44e99c813fb715e61fa454f28e521
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-18 04:06:00 +0900

    #BOJ 멀티탭 스케줄링

--- BOJ/Greedy/1931.py
+++ BOJ/Greedy/1931.py
@@ -1,5 +1,7 @@
-#https://www.acmicpc.net/problem/1931
-#회의실배정
+#https://www.acmicpc.net/problem/1700
+#https://hongcoding.tistory.com/22
+#1.앞으로 사용할 예정에 없음 2.제일 나중에 쓸거임 순서로 뽑는다
+#멀티탭 스케줄링
 ##내 풀이->테스트 통과 DATE=>5.17 풀이시간 60분 
 n=int(input())
 meetings=[]
