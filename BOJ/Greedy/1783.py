4a7b82a5f1607de76c35d9f454140c8ba4b49b38
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-20 04:40:20 +0900

    #BOJ 병든나이트

--- BOJ/Greedy/1783.py
+++ BOJ/Greedy/1783.py
@@ -21,3 +21,20 @@ else:
     else:
         ans=3+(m-5)
 print(ans)
+
+'''
+min으로 코드 단축시키기
+N, M = map(int, input().split())
+if N == 1:
+    scount = 1
+elif N == 2:
+    scount = min(4, (M-1)//2 + 1)
+elif M < 7:
+    scount = min(4, M)
+else:
+    scount = (2 + (M-5)) + 1
+print(scount)
+
+
+
+'''
