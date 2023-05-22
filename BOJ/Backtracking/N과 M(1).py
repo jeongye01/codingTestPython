0273aa65cc5045cf63db620f762a720b56987fc1
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-23 03:01:07 +0900

    #BOJ N과 M (1)

---
+++ BOJ/Backtracking/N과 M(1).py
@@ -0,0 +1,15 @@
+#https://www.acmicpc.net/problem/15649
+#내 풀이->테스트 통과 DATE=>5.22 풀이시간 10분 
+n,m=map(int,input().split())
+
+def bt(arr):
+  if len(arr)==m:
+    print(*arr)
+    return
+  for i in range(1,n+1):
+    if i not in arr:
+      bt(arr+[i])
+    
+
+
+bt([])
