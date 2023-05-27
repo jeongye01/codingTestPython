6653de78cf86c4c28d9487a7b911caa4b9d9bb3c
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-28 03:54:50 +0900

    Create 약수개수빨리구하기.py

---
+++ 외워둘것들/약수개수빨리구하기.py
@@ -0,0 +1,9 @@
+def solution(number, limit, power):
+    answer = 0
+    for n in range(1,number+1):
+        cnt=0
+        for i in range(1,int(n**0.5)+1):
+            if n%i==0:
+                cnt+=2 if i**2!=n else 1
+        answer+=(cnt if cnt<=limit else power)                
+    return answer
