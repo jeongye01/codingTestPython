6f76164b75a7d804c72fcfbc30bff11029ee4fe5
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-06-12 12:46:28 +0900

    #프로그래머스 주식가격

---
+++ 프로그래머스/Lv2/Day4/주식가격.py
@@ -0,0 +1,19 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/42584
+#https://velog.io/@letgodchan0/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%A3%BC%EC%8B%9D%EA%B0%80%EA%B2%A9
+from collections import deque
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
