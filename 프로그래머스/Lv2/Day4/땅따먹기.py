935d1ee690a677919642012d125a200ef2513cc9
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-06-14 15:08:14 +0900

    Update 땅따먹기.py

--- 프로그래머스/Lv2/Day4/땅따먹기.py
+++ 프로그래머스/Lv2/Day4/땅따먹기.py
@@ -11,3 +11,15 @@ def solution(land):
             d[i][j] = land[i][j] + max(d[i-1][a], d[i-1][b], d[i-1][c])
 
     return max(d[-1])
+
+'''
+
+def solution(land):
+    answer = 0
+    for i in range(1, len(land)):
+        for j in range(4):
+            a, b, c = [jj for jj in range(4) if jj != j]
+            land[i][j] += max(land[i-1][a], land[i-1][b], land[i-1][c])
+    return max(land[-1])
+
+'''
